06a767aede61091bde896cf12a20193eb07116f3
Omar Alhalaika <omar.alhalaika@yahoo.com>
2021-03-01 16:19:28 +0200

    to submit the lab

---
+++ js/app.js
@@ -0,0 +1,26 @@
+alert(`Welcome to the COLORFUL WORLD`)
+let userName=prompt(`Enter your name sweetie \nso you can start the journey `)
+alert( `welcome to Stoneood  `+userName )
+let age=prompt(`How many years you count so far in your life!`)
+alert(`congratulations! so far you have reached level `+age+` of Jumanji `)
+let job=prompt(`What is your dream job?`)
+alert( `Greetings to the future `+job)
+let userColor=prompt(`What is your favorite color\nKindly chose one of those\n(Blue, Purple, Red, Pink, Orange, Yellow, Green, White, Black)`)
+alert(`Let's start this level with your favorite `+userColor)
+
+
+
+console.log(`username: ` + userName)
+console.log(`user's age: ` + age)
+console.log(`user's job: ` + job)
+console.log(`user's color: ` + userColor)
+
+
+//`\nwelcome to Stoneood `+userName+
+//`\ncongratulations! so far you have reached level `+age+`of Jumanji`
+//`\nLet's start this level with your favorite `+userColor
+//)
+//document.write( `<h1>Greetings to the future `+job+`</h1>`)
+//document.writeln( `<h1> welcome to Stoneood  `+userName +`</h1>`)
+//document.writeln(`<h1>congratulations! so far you have reached level `+age+`of Jumanji </h1>`)
+//document.writeln(`<h1>Let's start this level with your favorite `+userColor+`</h1>`)
